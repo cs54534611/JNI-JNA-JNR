50387c68467a5b25b8dfba09a86513d5e0086502
cs54534611 <53256682+cs54534611@users.noreply.github.com>
2022-05-12 11:43:12 +0800

    Add files via upload

---
+++ JNA/JNATEST/JNATEST.h
@@ -0,0 +1,9 @@
+#ifdef JNATEST_EXPORTS
+#define JNATEST_API __declspec(dllexport)
+#else
+#define JNATEST_API __declspec(dllimport)
+#endif
+extern "C" {
+	JNATEST_API void set(long l);
+	JNATEST_API long get();
+}
